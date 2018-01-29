3e117b9bfd7e5ba0d7341f2e4f7508f1e827bcb2
rwilliams15 <rwilliams15@ggc.edu>
2018-01-29 17:13:23 -0500

    Update index.js
    Math to calculate the temp for Celsius and Fahrenheit. Also a link to get the map icons.

--- js/index.js
+++ js/index.js
@@ -30,3 +30,32 @@ console.log(data);
 	
 	//openweathermap api with my APIID
     $.getJSON("https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=355cf4f52f81b3451ddb5d55e700b906", function(weatherData)
+	      {
+      $.each(weatherData, function(j, b) {});
+      console.log(weatherData);
+      // fetch image and display weather and icon down below 
+      tWeather = weatherData.main.temp;
+      iconNumber = weatherData.weather[0].icon;
+
+      icon = ("http://openweathermap.org/img/w/" + iconNumber + ".png");
+      $("#graphic").append('<img src="' + icon + '"/>');
+
+      $('#description').append(weatherData.weather[0].description);
+      $('#city').append(weatherData.name);
+      minF = Math.floor(weatherData.main.temp_min * 9/5 - 459.67);
+      maxF = Math.floor(weatherData.main.temp_max * 9/5 - 459.67);
+
+      // translate Kelvin to Celsius
+      celsius = Math.floor(weatherData.main.temp - 273.15);
+
+      minCelsius = Math.floor(weatherData.main.temp_min - 273.15);
+      maxCelsius = Math.floor(weatherData.main.temp_max - 273.15);
+      // translate Kelvin to Farenhait T(K) × 9/5 - 459.67
+      farenheit = Math.floor(weatherData.main.temp * 9 / 5 - 459.67);
+
+      // display celsius by default 
+      $('#celsius').append(celsius + 'ºC');
+      $("#min").append( minCelsius + 'ºC');
+      $("#max").append( maxCelsius + 'ºC');
+    });
+  
