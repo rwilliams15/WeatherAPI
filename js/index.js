5188fcbb87cfe69a6739837ff744ac361690a7c3
rwilliams15 <rwilliams15@ggc.edu>
2018-01-29 17:11:38 -0500

    Update index.js
    api data added with long and lat to find users location

--- js/index.js
+++ js/index.js
@@ -10,3 +10,23 @@ var lat,
   maxCelsius,
   minF,
   maxF;
+
+//api that gets your ip for your location
+$.getJSON('https://ipapi.co/json/', function(data)
+{
+  console.log(data);
+
+
+  // reaching the location API
+
+console.log(data);
+    // Finding the latitude and longitude, to be able to find the location
+    $.each(data, function(k, v) {});
+    lat = data.latitude;
+    lon = data.longitude;
+
+    $("#latitude").append(lat);
+    $("#longitude").append(lon);
+	
+	//openweathermap api with my APIID
+    $.getJSON("https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=355cf4f52f81b3451ddb5d55e700b906", function(weatherData)
